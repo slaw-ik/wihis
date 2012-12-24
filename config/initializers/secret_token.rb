d53c2d33fc41807c8405395bc842355188e42e21
slaw-ik <slaw-ik@yandex.ru>
2012-12-24 22:03:44 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wihis::Application.config.secret_token = '6979320c3aab1afa61df145717c075e7a0c6d84c30d2d66afab0ff78485560f087afaa301ff846c0da97f0c9f7ee3334ad485e9cc21f7a17085f704a664b6256'
